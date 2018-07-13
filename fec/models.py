450165c90c4a2c595db7b13b545a160d62a66d21
rshorey <rshorey@gmail.com>
2018-07-13 14:17:39 -0400

    store donor total directly on donor

--- fec/models.py
+++ fec/models.py
@@ -241,16 +241,13 @@ class Donor(BaseModel):
     nyt_note = models.TextField(null=True, blank=True)
     city = models.CharField(max_length=255, null=True, blank=True)
     state = models.CharField(max_length=255, null=True, blank=True)
-
-    @property
-    def contribution_total(self):
-        try:
-            amount = self.schedulea_set.aggregate(Sum('contribution_amount'))['contribution_amount__sum']
-            if amount:
-                return amount
-            return 0
-        except:
-            return 0
+    contribution_total = models.DecimalField(max_digits=12,decimal_places=2, null=True, blank=True)
+    
+    def save(self, *args, **kwargs):
+        self.contribution_total = self.schedulea_set.aggregate(Sum('contribution_amount'))['contribution_amount__sum']
+        if not self.contribution_total:
+            self.contribution_total = 0
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.nyt_name
